2bbfae1dbcc0915fcaca512fe7dd5b8403e6700a
ste1v0 <tyotarjous@yandex.ru>
2023-09-27 18:25:08 +0200

    init: basic structure

---
+++ password-generator/index.js
@@ -0,0 +1,24 @@
+const characters = 
+["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+// console.log(characters.length)
+
+let passwordOne = document.querySelector(".passwordOne")
+let passwordTwo = document.querySelector(".passwordTwo")
+
+function generate() {
+    let firstPassword = ''
+    for (i = 0; i < 12; i++) {
+        firstPassword += characters[Math.floor(Math.random() * characters.length)]
+    }
+    passwordOne.textContent = firstPassword
+    
+    let secondPassword = ''
+    for (i = 0; i < 12; i++) {
+        secondPassword += characters[Math.floor(Math.random() * characters.length)]
+    }
+    passwordTwo.textContent = secondPassword
+}
+
+
