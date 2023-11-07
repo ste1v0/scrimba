437d28c1ff558b264895ba7a8879f256f9c31f35
ste1v0 <tyotarjous@yandex.ru>
2023-11-07 21:39:34 +0100

    refact: minor improvements

--- movie-watchlist/index.js
+++ movie-watchlist/index.js
@@ -6,7 +6,6 @@ const main = document.getElementById('main')
 
 async function findMovie(event) {
         event.preventDefault()
-
         if (main.classList.contains('height')) {
             main.classList.remove('height')
         }
@@ -21,11 +20,12 @@ async function findMovie(event) {
 }
 
 async function fetchDetails(movies) {
+    let result = ''
     for (let movie of movies) {
         const res = await fetch(`${apiURL}?apikey=${apiKEY}&t=${movie}`)
         const data = await res.json()
 
-            let gridItem = `
+            result += `
             <div class="grid-item">
                 <img class="poster" src="${data.Poster}">
                 <p class="title">${data.Title} <img class="favorite" src="./img/favorite.svg"> <span class="rating">${data.imdbRating}</span></p>
@@ -37,8 +37,8 @@ async function fetchDetails(movies) {
             </div>
             <hr>
             `
-            document.querySelector('#grid-container').innerHTML += gridItem
-            }
+        }
+    document.querySelector('#grid-container').innerHTML += result
 }
 
 searchBtn.addEventListener('click', findMovie)
