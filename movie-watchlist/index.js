c58e4075b8ca356ad95a4583296949b29af17e45
ste1v0 <tyotarjous@yandex.ru>
2023-11-07 21:00:07 +0100

    refact: multiple fixes and refactoring

--- movie-watchlist/index.js
+++ movie-watchlist/index.js
@@ -4,44 +4,42 @@ const apiURL = 'https://www.omdbapi.com'
 const apiKEY = 'cd8d3209'
 const main = document.getElementById('main')
 
-let movies = []
+async function findMovie(event) {
+        event.preventDefault()
 
-searchBtn.addEventListener('click', async function(event) {
-    document.getElementById('explore-text').classList.add('hidden')
-    document.getElementById('explore-pic').classList.add('hidden')
-    movies = []
-    event.preventDefault()
-    main.classList.remove('height')
-    await fetch(`${apiURL}?apikey=${apiKEY}&s=${searchInput.value}`)
-        .then(res => res.json())
-        .then(data => {
-                data.Search.map(e => movies.push(e.Title))
-        })
+        if (main.classList.contains('height')) {
+            main.classList.remove('height')
+        }
 
+        const res = await fetch(`${apiURL}?apikey=${apiKEY}&s=${searchInput.value}`)
+        const data = await res.json()
+    
+        const movies = data.Search.map(e => e.Title)
+    
         main.innerHTML = `<div id="grid-container"></div>`
+        fetchDetails(movies)
+}
 
-        for (let movie of movies) {
-            await fetch(`${apiURL}?apikey=${apiKEY}&t=${movie}`)
-                .then(res => res.json())
-                .then(data => {
+async function fetchDetails(movies) {
+    for (let movie of movies) {
+        const res = await fetch(`${apiURL}?apikey=${apiKEY}&t=${movie}`)
+        const data = await res.json()
 
-                let gridContainer = `
-                <div class="grid-item">
-                    <img class="poster" src="${data.Poster}">
-                    <p class="title">${data.Title} <img class="favorite" src="./img/favorite.svg"> <span class="rating">${data.imdbRating}</span></p>
-                    <p class="duration">${data.Runtime}</p>
-                    <p class="genres">${data.Genre}</p>
-                    <img class="add-icon" src="./img/add.svg">
-                    <p class="add-watchlist-text">Watchlist</p>
-                    <p class="description">${data.Plot}</p>
-                </div>
-                <hr>
-                `
+            let gridItem = `
+            <div class="grid-item">
+                <img class="poster" src="${data.Poster}">
+                <p class="title">${data.Title} <img class="favorite" src="./img/favorite.svg"> <span class="rating">${data.imdbRating}</span></p>
+                <p class="duration">${data.Runtime}</p>
+                <p class="genres">${data.Genre}</p>
+                <img class="add-icon" src="./img/add.svg">
+                <p class="add-watchlist-text">Watchlist</p>
+                <p class="description">${data.Plot}</p>
+            </div>
+            <hr>
+            `
+            document.querySelector('#grid-container').innerHTML += gridItem
+            }
+}
 
-                document.querySelector('#grid-container').innerHTML += gridContainer
-        
-                })}
-                
-
-
-})
+searchBtn.addEventListener('click', findMovie)
+            
