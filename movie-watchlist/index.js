a9bb98b1b8763a2a8db0a4727f736c4caff1e350
ste1v0 <tyotarjous@yandex.ru>
2023-11-08 09:58:30 +0100

    feat: remove watchlist items

--- movie-watchlist/index.js
+++ movie-watchlist/index.js
@@ -4,6 +4,8 @@ const apiURL = 'https://www.omdbapi.com'
 const apiKEY = 'cd8d3209'
 const main = document.getElementById('main')
 
+let movieData = []
+
 async function findMovie(event) {
         event.preventDefault()
         if (main.classList.contains('height')) {
@@ -21,9 +23,11 @@ async function findMovie(event) {
 
 async function fetchDetails(movies) {
     let result = ''
-    for (let movie of movies) {
+    for (let [index, movie] of movies.entries()) {
         const res = await fetch(`${apiURL}?apikey=${apiKEY}&t=${movie}`)
         const data = await res.json()
+
+        movieData[index] = data
 
             result += `
             <div class="grid-item">
@@ -31,15 +35,25 @@ async function fetchDetails(movies) {
                 <p class="title">${data.Title} <img class="favorite" src="./img/favorite.svg"> <span class="rating">${data.imdbRating}</span></p>
                 <p class="duration">${data.Runtime}</p>
                 <p class="genres">${data.Genre}</p>
-                <img class="add-icon" src="./img/add.svg">
-                <p class="add-watchlist-text">Watchlist</p>
+                <img data-id="${index}" class="add-icon" src="./img/add.svg">
+                <p data-id="${index}" class="add-watchlist-text">Watchlist</p>
                 <p class="description">${data.Plot}</p>
             </div>
             <hr>
             `
         }
     document.querySelector('#grid-container').innerHTML += result
+
+    const buttons = document.querySelectorAll('.add-icon')
+    buttons.forEach(e => {
+        e.addEventListener('click', function(event) {
+            const buttonId = parseInt(event.target.getAttribute('data-id'), 10)
+            localStorage.setItem(`movieData-${buttonId}`, JSON.stringify(movieData[buttonId]))
+        })
+    })
 }
 
 searchBtn.addEventListener('click', findMovie)
+
+
             
---
+++ movie-watchlist/watchlist.js
@@ -0,0 +1,47 @@
+const main = document.getElementById('main')
+const allKeys = Object.keys(localStorage)
+
+let result = ''
+
+if (allKeys) {
+    if (main.classList.contains('height')) {
+        main.classList.remove('height')
+    }
+    main.innerHTML = `<div id="grid-container"></div>`
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i)
+        const value = localStorage.getItem(key)
+        const parsed = JSON.parse(value)
+
+        result += `
+        <div class="grid-item">
+            <img class="poster" src="${parsed.Poster}">
+            <p class="title">${parsed.Title} <img class="favorite" src="./img/favorite.svg"> <span class="rating">${parsed.imdbRating}</span></p>
+            <p class="duration">${parsed.Runtime}</p>
+            <p class="genres">${parsed.Genre}</p>
+            <img data-id="${i}" class="add-icon" src="./img/remove.svg">
+            <p data-id="${i}" class="add-watchlist-text">Remove</p>
+            <p class="description">${parsed.Plot}</p>
+        </div>
+        <div class="hr">
+            <hr>    
+        </div>
+
+        `
+    }
+    document.querySelector('#grid-container').innerHTML += result
+
+    const buttons = document.querySelectorAll('.add-icon')
+    buttons.forEach(e => {
+        e.addEventListener('click', function(event) {
+            const buttonId = parseInt(event.target.getAttribute('data-id'), 10)
+            localStorage.removeItem(`movieData-${buttonId}`)
+            event.target.closest('.grid-item').remove();
+            event.target.closest('.hr').remove();
+        })
+    })
+}
+
+
+
+            
